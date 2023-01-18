c675d8cc8ec008e8be196320b8eae2472d6f8167
Farouq Siyar <siyar9989@gmail.com>
2023-01-18 16:59:02 +0100

    start working on userPage

--- src/Components/UserAvatar.jsx
+++ src/Components/UserAvatar.jsx
@@ -1,3 +1,3 @@
-export default function UserAvatar({username}) {
-    return <div className="letter-avatar">{username.slice(0, 1)}</div>
+export default function UserAvatar({username, isBigSize=false}) {
+    return <div className= {isBigSize ? "letter-avatar-big" : "letter-avatar"}>{username.slice(0, 1)}</div>
 }
--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -1,7 +1,42 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import UserAvatar from "../../Components/UserAvatar";
+import { useEffect, useState } from "react";
+import Loading from "../../Components/Loading";
+import "./UserPage.css";
 
 export default function UserPage() {
-    const {id} = useParams();
+  const { id } = useParams();
+  const [userPageDTO, setUserPageDTO] = useState(null);
 
-    return <>User no. {id}</>
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/api/user/" + id);
+      const data = await response.json();
+      setUserPageDTO(data);
+    }
+    fetchData();
+  }, []);
+
+  return userPageDTO ? (
+    <>
+    <div className="user-card">
+      <div className="user-avatar">
+        <UserAvatar username={userPageDTO.name} isBigSize />
+      </div>
+      <div className="user-details">
+      <div className="user-name">{userPageDTO.name}</div>
+      <div className="user-registration">Joined: <i>{userPageDTO.registration.split("T").join(" ")}</i></div>
+      <div className="user-questions">Questions: <i>{userPageDTO.questions}</i></div>
+      <div className="user-answers">Answers: <i>{userPageDTO.answers}</i></div>
+      </div>
+    </div>
+    
+    <div className="options">
+    <div className="questions-button clickable">Questions</div>
+    <div className="answers-button clickable">Answers</div>
+    </div>
+    </>
+  ) : (
+    <Loading />
+  );
 }
