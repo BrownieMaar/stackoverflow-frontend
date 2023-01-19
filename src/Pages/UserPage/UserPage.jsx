d0bdcd2d6686d164f6e87a590875b6eb03fb1525
Farouq Siyar <siyar9989@gmail.com>
2023-01-19 15:00:44 +0100

    implement questions and answers option for userPage

--- src/Pages/UserPage/UserPage.jsx
+++ src/Pages/UserPage/UserPage.jsx
@@ -3,11 +3,20 @@ import UserAvatar from "../../Components/UserAvatar";
 import { useEffect, useState } from "react";
 import Loading from "../../Components/Loading";
 import "./UserPage.css";
+import QuestionsOption from "./QuestionsOption";
+import AnswersOption from "./AnswersOption";
 
 export default function UserPage() {
   const { id } = useParams();
   const [userPageDTO, setUserPageDTO] = useState(null);
+  const [activeOption, setActiveOption] = useState("Questions");
   const navigate = useNavigate();
+
+  function handleOptions(e) {
+    e.target.innerText === "Questions"
+      ? setActiveOption("Questions")
+      : setActiveOption("Answers");
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -21,7 +30,7 @@ export default function UserPage() {
   const signOut = () => {
     window.currentUser = null;
     history.back();
-  }
+  };
 
   return userPageDTO ? (
     <>
@@ -31,15 +40,40 @@ export default function UserPage() {
         </div>
         <div className="user-details">
           <div className="user-name">{userPageDTO.name}</div>
-          <div className="user-registration">Joined: <i>{new Date(userPageDTO.registration).toLocaleString()}</i></div>
-          <div className="user-questions">Q/A: <span className="qa-ratio"><span>{userPageDTO.questions}</span> / <span>{userPageDTO.answers}</span></span></div>
+          <div className="user-registration">
+            Joined: <i>{new Date(userPageDTO.registration).toLocaleString()}</i>
+          </div>
+          <div>
+            Q/A:{" "}
+            <span className="qa-ratio">
+              <span>{userPageDTO.questions}</span> /{" "}
+              <span>{userPageDTO.answers}</span>
+            </span>
+          </div>
         </div>
       </div>
 
       <div className="options">
-        <div className="button clickable">Questions</div>
-        <div className="button clickable">Answers</div>
-        {window.currentUser && window.currentUser.id === userPageDTO.id ? <div className="button clickable" onClick={signOut}>Sign Out</div> : <></> }
+        <div className="button clickable" onClick={(e) => handleOptions(e)}>
+          Questions
+        </div>
+        <div className="button clickable" onClick={(e) => handleOptions(e)}>
+          Answers
+        </div>
+        {window.currentUser && window.currentUser.id === userPageDTO.id ? (
+          <div className="button clickable" onClick={signOut}>
+            Sign Out
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div className="user-option">
+        {activeOption === "Questions" ? (
+          <QuestionsOption user={userPageDTO} />
+        ) : (
+          <AnswersOption user={userPageDTO} />
+        )}
       </div>
     </>
   ) : (
