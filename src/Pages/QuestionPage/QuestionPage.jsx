23c65b861d60661880f26a7b53d94de3963ed7b0
BrownieMaar <barnamarci07@gmail.com>
2023-01-19 07:06:08 +0100

    add new color variable for input backgrounds

--- src/Pages/QuestionPage/QuestionPage.jsx
+++ src/Pages/QuestionPage/QuestionPage.jsx
@@ -5,24 +5,29 @@ import AnswerCard from "./AnswerCard";
 import QuestionCardDetailed from "./QuestionCardDetailed";
 
 export default function QuestionPage() {
-    const { id } = useParams();
-    const [questionDTO, setQuestionDTO] = useState(null);
+  const { id } = useParams();
+  const [questionDTO, setQuestionDTO] = useState(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            const response = await fetch('/api/questions/' + id);
-            const data = await response.json();
-            console.log(data);
-            setQuestionDTO(data);
-        }
-        fetchData();
-      }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/api/questions/' + id);
+      const data = await response.json();
+      console.log(data);
+      setQuestionDTO(data);
+    }
+    fetchData();
+  }, []);
 
-    return questionDTO ?
-     <div className="questionpage-cards">
-     <QuestionCardDetailed questionPageDTO={questionDTO} />
-     {questionDTO.answers.map(answer => <AnswerCard key={answer.id} answerDTO={answer} /> )}
-     </div>
-     :
-     <Loading />
+  return questionDTO ?
+    <div className="questionpage-cards">
+      <QuestionCardDetailed questionPageDTO={questionDTO} />
+      {questionDTO.answers.map(answer => <AnswerCard key={answer.id} answerDTO={answer} />)}
+
+      <div className="card answer">
+        <div className="answer user">Add new question:</div>
+
+      </div>
+    </div>
+    :
+    <Loading />
 }
